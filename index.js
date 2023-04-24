9bfe5a1a9d7f423e26e94ad17ca4220a3b465f28
khalil <khalillakhdharatc@gmail.com>
2023-04-24 11:56:14 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+// appel de express 
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies les formulaires
+app.use(bodyParser.json()); // support json encoded bodies les json api REST
+
+const port=4000;
+app.get('/', (req, res) => {
+    // http://localhost:4000/
+    res.send('Hello JS!');
+});
+app.get('/hello/:nom/:prenom',(req,res) => {
+res.send(`Hello ${req.params.nom} ${req.params.prenom}!`);
+});
+
+// post testing
+app.post('/send',(req,res) => {
+    console.log(req.body);
+    res.send(req.body);
+})
+app.put('/update',(req,res) => {
+    console.log(req.body);
+    res.send(req.body);
+})
+app.listen(port, () => {
+    console.log(`Server started on port: ${port}`);
+});
